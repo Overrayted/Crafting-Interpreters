7274db62f76839ac41da32c48adc83e3184c5943
Rayhan Mohamed <rmoha2@uwo.ca>
2026-05-21 21:23:18 -0400

    chapter 4 main content complete

--- src/com/craftinginterpreters/lox/Scanner.java
+++ src/com/craftinginterpreters/lox/Scanner.java
@@ -8,12 +8,35 @@ import java.util.Map;
 import static com.craftinginterpreters.lox.TokenType.*;
 
 
+
 public class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
     private int start = 0;
     private int current = 0;
     private int line = 1;
+
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",    AND);
+        keywords.put("class",  CLASS);
+        keywords.put("else",   ELSE);
+        keywords.put("false",  FALSE);
+        keywords.put("for",    FOR);
+        keywords.put("fun",    FUN);
+        keywords.put("if",     IF);
+        keywords.put("nil",    NIL);
+        keywords.put("or",     OR);
+        keywords.put("print",  PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super",  SUPER);
+        keywords.put("this",   THIS);
+        keywords.put("true",   TRUE);
+        keywords.put("var",    VAR);
+        keywords.put("while",  WHILE);
+    }
 
     //constructor
     Scanner(String source) {
@@ -78,10 +101,60 @@ public class Scanner {
                 line++;
                 break;
 
+            case '"': string(); break;
+
             default:
-                Lox.error(line, "Unexpected character.");
+                if(isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)){
+                    identifier();
+                } else {
+                        Lox.error(line, "Unexpected character.");
+                    }
                 break;
         }
+    }
+
+    // used to determine if it is an identifier or keyword
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type = IDENTIFIER;
+        addToken(type);
+    }
+
+    private void number() {
+        while (isDigit(peek())) {advance();}
+
+        //look for fractional part
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance();
+
+            while (isDigit(peek())) {advance();}
+        }
+
+        addToken(NUMBER,Double.parseDouble(source.substring(start,current)));
+    }
+
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') {line++;}
+            advance();
+        }
+
+        if(isAtEnd()) {
+            Lox.error(line, "Unterminated string.");
+            return;
+        }
+
+        // to consume closing ".
+        advance();
+
+        // Trim surrounding quotes.
+        String value = source.substring(start+1,current-1);
+        addToken(STRING,value);
     }
 
     private boolean match(char expected) {
@@ -98,19 +171,43 @@ public class Scanner {
         return source.charAt(current);
     }
 
+    // returns empty char if there is no character past or returns the next character
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
 
+    // is alpha or is digit
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    // is a letter or underscore lol
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+    }
+
+    //checks the character is a number lol
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    //checks if the file end has been reached
         private boolean isAtEnd() {
         return current >= source.length();
     }
 
+    //moves the scanner to next char
     private char advance() {
         return source.charAt(current++);
     }
 
+    //adds token
     private void addToken(TokenType type) {
         addToken(type, null);
     }
 
+    //adds token literal
     private void addToken(TokenType type, Object literal) {
         String text = source.substring(start, current);
         tokens.add(new Token(type, text, literal, line));
